13153b74c02dbf1a1e6f16422562bc3856c39a3f
Zach Ahn <engineering@zachahn.com>
2017-05-04 21:57:59 -0400

    Fix linter error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'transmog'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "transmog"
 
-require 'minitest/autorun'
+require "minitest/autorun"
