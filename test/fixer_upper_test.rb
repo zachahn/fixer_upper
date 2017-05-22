91ad5bb3b654a7abb182082875716d1fde81b665
Zach Ahn <engineering@zachahn.com>
2017-05-21 20:03:13 -0400

    Consolidate feature tests into unit tests
    There's quite a bit of overlap right now. I will split it up into
    individual feature specs when necessary

--- test/fixer_upper_test.rb
+++ test/fixer_upper_test.rb
@@ -11,6 +11,26 @@ class FixerUpperTest < TestCase
     end
   end
 
+  class Rotate
+    def initialize(amount)
+      @amount = amount
+    end
+
+    def call(text)
+      n = "A".ord + @amount - 1
+      n_ = "A".ord + @amount
+      split = "#{n_.chr}-ZA-#{n.chr}"
+
+      text.tr("A-Za-z", split.upcase + split.downcase)
+    end
+  end
+
+  class Shout
+    def call(text)
+      text.upcase
+    end
+  end
+
   def test_multiple_keys
     fixer_upper = FixerUpper.new
     fixer_upper["txt", "text"] = Identity.new
@@ -24,5 +44,19 @@ class FixerUpperTest < TestCase
     fixer_upper.register("txt", "text", to: Identity.new, jk: true)
 
     assert_equal("", fixer_upper.diy("hi", "txt"))
+  end
+
+  def test_string_conversion
+    fixer_upper = FixerUpper.new
+    fixer_upper["rot13"] = Rotate.new(13)
+
+    assert_equal("<pnrfne>", fixer_upper.renovate("file.rot13", "<caesar>"))
+  end
+
+  def test_it_reads_file_contents
+    fixer_upper = FixerUpper.new
+    fixer_upper["txt"] = Shout.new
+
+    assert_equal("HI\n", fixer_upper.renovate("test/fixtures/tiny.txt"))
   end
 end
