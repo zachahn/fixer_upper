134bdcf30b697607e3b1351f22106869b3fbbc59
Zach Ahn <engineering@zachahn.com>
2017-05-23 08:22:10 -0400

    Pass in filename to Sass::Engine when available

--- lib/fixer_upper/engine/scss.rb
+++ lib/fixer_upper/engine/scss.rb
@@ -1,8 +1,8 @@
 class FixerUpper
   module Engine
     class Scss
-      def call(text, **options)
-        options = options.merge(syntax: :scss)
+      def call(text, _filepath_:, **options)
+        options = options.merge(syntax: :scss, filename: _filepath_)
 
         engine = ::Sass::Engine.new(text, **options)
         engine.render
--- test/fixer_upper/engine/scss_test.rb
+++ test/fixer_upper/engine/scss_test.rb
@@ -9,4 +9,18 @@ class EngineScssTest < TestCase
 
     assert_equal("body {\n  color: #ccc; }\n", output)
   end
+
+  def test_has_filename_in_errors
+    fu = FixerUpper.new
+    fu.register("scss", to: FixerUpper::Engine::Scss.new)
+
+    error =
+      begin
+        fu.renovate("path/to/foo.scss", "body { color: $color }")
+      rescue Sass::SyntaxError => e
+        e
+      end
+
+    assert_equal("path/to/foo.scss", error.sass_filename)
+  end
 end
